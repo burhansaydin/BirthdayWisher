c3b4638334199354ae3860df45fdf044739b8bad
burhansaydin <burhasaydin@gmail.com>
2020-12-18 01:52:05 +0200

    -firstly imported all required classes -to control the birthday created for loop -created sen_mail function to sent birthday message when the current date matched with the birthdays in the birthdays.csv -and each message will be sent randomly with random.choice method in sent_mail function. - all the required info to sent mail extracted from birthday.csv according to birthday owner.

--- main.py
+++ main.py
@@ -1,10 +1,49 @@
 import smtplib
+import datetime as dt
+import random
+import pandas
 
-my_email = "qazxsw123@gmail.com"
-my_password = "qazxsw123"
+my_email = "qazxswqaz04@gmail.com"
+my_password = "qetuo123"
+s_mail_password = "qetuo123."
+name = ""
+mail = ""
 
-with smtplib.SMTP("smtp.gmail.com") as connection:
-    connection.starttls()
-    connection.login(user=my_email, password=my_password)
-    connection.sendmail(from_addr=my_email, to_addrs="qwerasdf123@yahoo.com",
-                        msg="Subject:Hello\n\n This is the body of my mail. ")
+datas = pandas.read_csv("birthdays.csv")
+d_dic = datas.to_dict()
+days = datas["day"].tolist()
+months = datas["month"].tolist()
+names = datas["name"].tolist()
+mails = datas["email"].tolist()
+
+now = dt.datetime.now()
+day = now.day
+month = now.month
+
+l_letter = [r"C:\Users\Lenovo\PycharmProjects\BirthdayWisher\letter_templates\letter_1.txt",
+            r"C:\Users\Lenovo\PycharmProjects\BirthdayWisher\letter_templates\letter_2.txt",
+            r"C:\Users\Lenovo\PycharmProjects\BirthdayWisher\letter_templates\letter_3.txt"]
+
+
+def sent_mail():
+    letter = random.choice(l_letter)
+
+    with open(letter) as f:
+        l = f.read()
+        l = l.replace("[NAME]", name)
+
+    with smtplib.SMTP("smtp.gmail.com") as connection:
+        connection.starttls()
+        connection.login(user=my_email, password=my_password)
+        connection.sendmail(
+            from_addr=my_email,
+            to_addrs=mail,
+            msg=f"Subject:Hello\n\n {l}"
+        )
+
+
+for i in range(0, len(days)):
+    if days[i] == day and months[i] == month:
+        name = names[i]
+        mail = mails[i]
+        sent_mail()
